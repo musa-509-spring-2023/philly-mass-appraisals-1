8dbddb01cd71845a508aa5cb7cd41dc41a210779
Yesao <673392438@qq.com>
2023-04-05 12:25:54 -0400

    Reviewer layout

--- ui/reviewer/js/map.js
+++ ui/reviewer/js/map.js
@@ -3,7 +3,26 @@ mapboxgl.accessToken = 'pk.eyJ1IjoieWVzZW5pYW8iLCJhIjoiY2tlZjAyM3p5MDNnMjJycW85b
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+    style: 'mapbox://styles/mapbox/light-v9', // style URL
     center: [-75.116728, 39.993436], // starting position [lng, lat],
     zoom: 11.2, // starting zoom
 });
+
+window.map = map;
+
+let mydata = {};
+
+// $.ajax({
+//     url: "https://phl.carto.com/api/v2/sql?filename=opa_properties_public&format=geojson&skipfields=cartodb_id&q=SELECT+*+FROM+opa_properties_public",
+//     type: "GET",
+//     dataType: "json",
+//     async: false,
+//     success: function(data) {
+//         mydata = data;
+//     },
+//     error: function (err) {
+//         console.log(err);
+//     },
+// });
+//
+// console.log(mydata)
