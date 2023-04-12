21041e4383f58cae2340dad503692e5bf5df0021
Rebekah Adams <rebekah.adams15@gmail.com>
2023-04-12 12:00:25 -0400

    fixed file names
    - changed file names to main.py
    - changed json download to skip string step, making it more efficient

--- src/extract_properties/main.py
+++ src/extract_properties/main.py
@@ -9,9 +9,7 @@ def extract_data(request):
     bucket = client.bucket('musa509s23_team01_raw_data') 
     url = 'https://phl.carto.com/api/v2/sql?filename=opa_properties_public&format=geojson&skipfields=cartodb_id&q=SELECT+*+FROM+opa_properties_public'
     resp = requests.get(url)
-    data = resp.json()
-
-    json_data = json.dumps(data)
+    json_data = resp.text()
     blob = bucket.blob('phl_opa_properties/opa_properties.json')
     blob.upload_from_string(json_data, content_type='application/json')
 
