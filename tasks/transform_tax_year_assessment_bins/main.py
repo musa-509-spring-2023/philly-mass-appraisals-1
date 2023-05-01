9a0792375134fc7025797797561b6399b8db0896
Mjumbe Poe <mjumbewu@gmail.com>
2023-04-30 21:57:47 -0400

    Move code into a function package
    - Created a folder with a requirements.txt
    - In the Python script:
      - Use the functions_framework.http decorator (this seems like it's technically optional, as the framework will treat your function as an HTTP handler by default, but still it's good to be explicit)
      - Specify the path to the SQL file relative to the script's folder; there's no guarantee that the script runner will know where to look for the file otherwise

--- tasks/transform_tax_year_assessment_bins/main.py
+++ tasks/transform_tax_year_assessment_bins/main.py
@@ -1,13 +1,19 @@
 # The python code for cloud function (Task 07)
 
 import os
+import pathlib
+import functions_framework
 from google.cloud import bigquery
 
+ROOT = pathlib.Path(__file__).parent
+
+
+@functions_framework.http
 def generate_derived_table(request):
     project_id = os.environ['GCP_PROJECT']
     
     # Read the SQL query from the file
-    with open('create_derived_tax_year_assessment_bins.sql', 'r') as sql_file:
+    with open(ROOT / 'create_derived_tax_year_assessment_bins.sql', 'r') as sql_file:
         sql_query = sql_file.read()
     
     # Run the SQL query
